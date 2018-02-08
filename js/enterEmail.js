b5ce224f7786dd335639e8f63af1dccf46482ca3
Mike Solomon <msolomon@thrillist.com>
2018-02-07 21:07:17 -0500

    email form is working

--- js/enterEmail.js
+++ js/enterEmail.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
   $('#top_email').on('change', function(e) {
     e.preventDefault();
     $('#email').val( $('#top_email').val() )
+    $('#replyto').val( $('#top_email').val() )
+  })
+
+  // set replyto to value of email
+  $('#email').on('change', function(e) {
+    e.preventDefault();
+    $('#replyto').val( $('#email').val() )
   })
 
   // Scroll to the contact section when clicking Submit at the top of page
@@ -12,5 +19,8 @@ $(document).ready(function() {
     $(document).scrollTop( $("#contact").offset().top )
     $('#firstname').focus()
   })
+
+  // redirect form after email submissions
+  $('#form-redirect').val( window.location + 'thanks.html' )
 
 })
